37c410df22c8ece6b8965e1b31693d57624471e6
Mauh28 <maurogoku2005@gmail.com>
2026-09-03 11:28:36 -0600

    Initial commit

---
+++ src/main/java/com/hackaton/prog/ProgApplication.java
@@ -0,0 +1,15 @@
+package com.hackaton.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class ProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgApplication.class, args);
+	}
+
+}
